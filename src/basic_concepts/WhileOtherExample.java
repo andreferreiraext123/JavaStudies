0a7aaab8bad8367941ee151c287f1bf3c5193d1b
andreferreiraext123 <mf1903491@gmail.com>
2024-09-09 17:16:02 -0300

    Adding file WhileOtherExample.java from folder basic_concepts

---
+++ src/basic_concepts/WhileOtherExample.java
@@ -0,0 +1,14 @@
+package basic_concepts;
+
+public class WhileOtherExample {
+    public static void main(String[] args) {
+        int countDown = 3;
+
+        while (countDown > 0){
+            System.out.println(countDown);
+            countDown --; // this will to decrease one from variable
+        }
+        System.out.print("Happy new years eve!!!");
+
+    }
+}
