3ccbf43c80b6157ae14a916f87862af96601c51c
andreferreiraext123 <mf1903491@gmail.com>
2024-09-09 16:55:57 -0300

    Adding file WhileLoopExample.java from folder basic_concepts

---
+++ src/basic_concepts/WhileLoopExample.java
@@ -0,0 +1,12 @@
+package basic_concepts;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num <= 10){ // The code is executed while the condition is true
+            System.out.println(num); // Block of code that will be executed for each interation by while
+            num++;
+        }
+    }
+}
